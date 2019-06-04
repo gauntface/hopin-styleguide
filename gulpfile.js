8ed908b7c29c7c16192bd0675ff5cf0da77940b2
Matt Gaunt <mattgaunt@google.com>
2019-06-03 21:07:26 -0700

    Adding a gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const gulp = require('gulp');
 const fs = require('fs-extra');
+const express = require('express');
+const serveIndex = require('serve-index');
 const {setConfig} = require('@hopin/wbt-config');
 const tsNode = require('@hopin/wbt-ts-node'); 
 const tsBrowser = require('@hopin/wbt-ts-browser'); 
+const {execSync} = require('child_process');
 
 const src = path.join(__dirname, 'src');
 const dst = path.join(__dirname, 'build');
@@ -29,3 +32,49 @@ gulp.task('build',
     ),
   )
 );
+
+gulp.task('watch', () => {
+  const options = {
+    delay: 1000,
+    ignoreInitial: false,
+  };
+  startServer();
+  gulp.watch([
+    'src/**/*', 
+    'template/**/*',
+    '!template/build/**/*',
+  ],
+  options,
+  gulp.series(
+    'build',
+    'build-demo',
+  ),
+  );
+});
+
+gulp.task('build-demo', function() {
+  const stdOut = execSync(`node ./build/cli.js build --dir ~/Projects/Code/gauntface-theme`);
+  console.log(stdOut.toString());
+  return Promise.resolve();
+});
+
+const staticDir = path.resolve(__dirname, 'generated-styleguide');
+const PORT = 9000;
+const app = express();
+app.use(
+  express.static(staticDir),
+  serveIndex(staticDir, {'icons': true})
+);
+
+function startServer() {
+  return new Promise((resolve, reject) => {
+    app.listen(PORT, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        console.log(`The server is running @ http://localhost:${PORT}`);
+        resolve();
+      }
+    });
+  });
+}
